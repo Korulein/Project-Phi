1ab94f596342e6846861417ec0306eed7957260e
Hasha <daniel.trandafir2005@gmail.com>
2025-06-02 10:14:54 +0200

    continuation of the supplier screen

---
+++ Assets/Scripts/Desk/SuppliersManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuppliersManager : MonoBehaviour
+{
+    public static SuppliersManager instance { get; private set; }
+
+    [Header("Suppliers")]
+    [SerializeField] public List<GameObject> supplierScreens = new List<GameObject>();
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void OpenSupplier(int supplierIndex)
+    {
+        for (int i = 0; i < supplierScreens.Count; i++)
+        {
+            if (i != supplierIndex)
+                supplierScreens[i].SetActive(false);
+        }
+        supplierScreens[supplierIndex].SetActive(true);
+    }
+}
